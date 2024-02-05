c9b18e2b343e85978f07774d5e785a1fa3dd9b5b
RhaHyunAh <rhahyeona@kakao.com>
2024-02-05 14:06:29 +0900

    remove promise

--- backend/keywords.js
+++ backend/keywords.js
@@ -1,54 +1,51 @@
 const { VertexAI } = require("@google-cloud/vertexai");
 
 async function keywords(content) {
-  return new Promise(async (resolve, reject) => {
-    try {
-      async function createStreamChat(
-        projectId = "keyloud",
-        location = "us-central1",
-        model = "gemini-pro"
-      ) {
-        // Initialize Vertex with your Cloud project and location
-        const vertexAI = new VertexAI({
-          project: projectId,
-          location: location,
-        });
+  try {
+    async function createStreamChat(
+      projectId = "keyloud",
+      location = "us-central1",
+      model = "gemini-pro"
+    ) {
+      // Initialize Vertex with your Cloud project and location
+      const vertexAI = new VertexAI({ project: projectId, location: location });
 
-        // Instantiate the model
-        const generativeModel = vertexAI.preview.getGenerativeModel({
-          model: model,
-        });
+      // Instantiate the model
+      const generativeModel = vertexAI.preview.getGenerativeModel({
+        model: model,
+      });
 
-        const chat = generativeModel.startChat({});
-        chatInput1 = `Extract 5 key keywords or phrases from the following text: ${content}
+      const chat = generativeModel.startChat({});
+      chatInput1 = `Extract 5 key keywords or phrases from the following text: ${content}
         Please respond korean with the extracted keywords, prefixed with a dash (-).
         At this point, the extracted keywords may have different language formats.
         As an example, when extracting keywords from a sentence like '나는 apple을 바바나와 맛있게 먹었다' respond with keywords that match the language format of the input text, such as 'apple' and '바나나'.`;
 
-        const result1 = await chat.sendMessageStream(chatInput1);
-        let tmp = "";
-        for await (const item of result1.stream) {
-          const responseText = item.candidates[0].content.parts[0].text;
-          const cleanedResponse = responseText.replace(/\n/g, "");
-          console.log(cleanedResponse);
-          tmp = tmp + cleanedResponse;
-        }
-        const tmp2 = tmp
-          .split(/(?=[^a-zA-Z0-9가-힣\s])/)
-          .filter((word) => word.length > 0);
-        const keywords = tmp2.map(
-          (word) => `${word.replace(/^[^a-zA-Z0-9가-힣]+/, "").trim()}`
-        );
-        console.log(keywords);
+      console.log(`User: ${chatInput1}`);
 
-        resolve(keywords);
+      const result1 = await chat.sendMessageStream(chatInput1);
+      let tmp = "";
+      for await (const item of result1.stream) {
+        const responseText = item.candidates[0].content.parts[0].text;
+        const cleanedResponse = responseText.replace(/\n/g, ""); // 줄바꿈 문자 제거
+        console.log(cleanedResponse);
+        tmp = tmp + cleanedResponse;
       }
+      const tmp2 = tmp
+        .split(/(?=[^a-zA-Z0-9가-힣\s])/)
+        .filter((word) => word.length > 0);
+      const keywords = tmp2.map(
+        (word) => `${word.replace(/^[^a-zA-Z0-9가-힣]+/, "").trim()}`
+      );
+      console.log(keywords);
 
-      await createStreamChat();
-    } catch (error) {
-      reject(error);
+      resolve(keywords);
     }
-  });
+
+    return await createStreamChat();
+  } catch (error) {
+    throw error;
+  }
 }
 
 module.exports = keywords;
