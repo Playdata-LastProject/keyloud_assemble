918cbb76b675eca150e9a23a158fd8b473232e2d
RhaHyunAh <rhahyeona@kakao.com>
2024-02-13 00:04:41 +0900

    dealing audio

--- frontend/src/components/ScriptDisplay.js
+++ frontend/src/components/ScriptDisplay.js
@@ -11,6 +11,7 @@ const ScriptDisplay = () => {
   const [Content, setContents] = useState({});
   const [error, setError] = useState(null);
   const [splitedScript, spliting] = useState([]);
+  const [audioData, setAudioData] = useState(null);
 
   useEffect(() => {
     const fetchData = async () => {
@@ -31,6 +32,30 @@ const ScriptDisplay = () => {
 
     fetchData();
   }, [location.state]);
+
+  useEffect(() => {
+    const fetchAudioData = async () => {
+      try {
+        if (receivedData.filename) {
+          const response = await axios.get(
+            `http://52.78.157.198:5000/get_audio?filename=${encodeURIComponent(
+              receivedData.filename
+            )}`,
+            {
+              responseType: "blob", // 이진 데이터로 응답 받음
+            }
+          );
+          setAudioData(response.data);
+          setLoading(false);
+        }
+      } catch (error) {
+        setError(error);
+        setLoading(false);
+      }
+    };
+
+    fetchAudioData();
+  }, [receivedData]);
 
   const getContents = async (fileID) => {
     setLoading(true);
@@ -124,12 +149,9 @@ const ScriptDisplay = () => {
       <AudioPlayer
         autoPlay
         controls
-        src={`http://52.78.157.198:5000/get_audio?filename=${encodeURIComponent(
-          receivedData.filename
-        )}`}
+        src={URL.createObjectURL(audioData)} // 이진 데이터를 URL 객체로 변환하여 사용
         type={Content.mimeType} // MIME 타입 추가
         onPlay={() => console.log("Audio is playing")}
-        // 필요한 경우 추가적인 속성들을 설정할 수 있습니다.
       />
       <p>script: {Content.scripts}</p>
       <div className="file-actions">
