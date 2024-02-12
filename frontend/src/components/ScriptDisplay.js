3a2ff13d7492915a7565acdda3ca776a66a4ea54
RhaHyunAh <rhahyeona@kakao.com>
2024-02-13 01:28:32 +0900

    dealing audio

--- frontend/src/components/ScriptDisplay.js
+++ frontend/src/components/ScriptDisplay.js
@@ -40,7 +40,10 @@ const ScriptDisplay = () => {
         const response = await axios.get(
           `http://52.78.157.198:5000/get_audio?filename=${encodeURIComponent(
             location.state.data.filename
-          )}`
+          )}`,
+          {
+            responseType: "blob", // 이진 데이터로 응답 받음
+          }
         );
         setAudioData(response.data);
         setLoading(false);
@@ -80,6 +83,7 @@ const ScriptDisplay = () => {
 
   const handleEdit = (fileName) => {
     // TODO: 파일 수정 로직 추가
+    console.log(`Edit file: ${fileName}`);
 
     const editEndpoint = "http://52.78.157.198:5000/update_scripts";
 
@@ -113,6 +117,7 @@ const ScriptDisplay = () => {
     const isConfirmed = window.confirm("파일을 휴지통으로 옮기시겠습니까?");
     if (isConfirmed) {
       // TODO: 파일 삭제 로직 추가
+      console.log(`Delete file: ${filename}`);
 
       const deleteEndpoint = "http://52.78.157.198:5000/move_to_trash";
 
