3ea51db9787a4af5fb125f3e77cfddbc38bdb921
KyungBin7 <80757237+KyungBin7@users.noreply.github.com>
2024-02-14 07:40:38 +0900

    design7

--- frontend/src/components/KeywordSearch.js
+++ frontend/src/components/KeywordSearch.js
@@ -1,9 +1,10 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import axios from "axios";
 //import { Link } from "react-router-dom";
 import { useNavigate, useLocation } from "react-router-dom";
 import _ from "lodash";
 import "./styles/KeywordResult.css";
+import AnimatedCircle from "./AnimatedCircle";  // 추가된 부분
 
 const KeywordSearchPage = () => {
   const [searchKeyword, setSearchKeyword] = useState("");
@@ -13,8 +14,21 @@ const KeywordSearchPage = () => {
   const [selectedItem, setSelectedItem] = useState(null); // 선택된 아이템의 키 값을 저장하는 상태
   const [targetSynonyms, setTargetSynonyms] = useState([]);
   const [onlySearchResults, setOnlySearchResults] = useState([]);
+  const [isFirst, setFirst] = useState(true);
   const navigate = useNavigate();
   const location = useLocation();
+
+  useEffect(() => {
+    // 2초 후에 페이드 인 스타일 적용
+    const timer = setTimeout(() => {
+      const searchContainer = document.querySelector('.search-sector');
+      if (searchContainer) {
+        searchContainer.classList.add('fade-in');
+      }
+    }, 2000);
+
+    return () => clearTimeout(timer); // 컴포넌트가 언마운트되면 타이머 제거
+  }, []);
 
   const handleSearch = async () => {
     const currentPath = location.pathname;
@@ -40,6 +54,7 @@ const KeywordSearchPage = () => {
     }
 
     setLoading(false);
+    setFirst(false);
   };
 
   // 클릭 이벤트 핸들러 추가
@@ -55,24 +70,42 @@ const KeywordSearchPage = () => {
 
   return (
     <div>
-      <input
-        type="text"
-        placeholder="검색어를 입력하세요"
-        value={searchKeyword}
-        onChange={(e) => setSearchKeyword(e.target.value)}
-      />
-      <button onClick={handleSearch} disabled={loading}>
-        검색
-      </button>
+      <div className={`search-sector ${isFirst ? 'first-style' : 'second-style'}`}>
+        <input
+          className="keyword-search"
+          type="text"
+          placeholder="검색어를 입력하세요"
+          value={searchKeyword}
+          onChange={(e) => setSearchKeyword(e.target.value)}
+        />
+        <button className="search-btn" onClick={handleSearch} disabled={loading}>
+          검색
+        </button>
 
-      {loading && <p>검색 중...</p>}
-      {error && <p>{error}</p>}
+        {loading && <p>검색 중...</p>}
+        {error && <p>{error}</p>}
+      </div>
+      
 
+      {/* first 상태가 true일 때만 AnimatedCircle 렌더링 */}
+      {isFirst && (
+        <div className="circle-container">
+          <AnimatedCircle />
+        </div>
+      )}
+
+      
       {searchResults.length > 0 && (
         <div>
           <h2>검색 결과</h2>
           <h3>
-            * 추천 검색어: {targetSynonyms[0]}, {targetSynonyms[1]}
+          <div className="recommend">
+            <img
+              src="/images/search.ico"
+              alt="키워드 아이콘"
+              className="search-icon"
+            /> 추천 검색어: {targetSynonyms[0]}, {targetSynonyms[1]}
+          </div>
           </h3>
           {onlySearchResults.map((results, upperIndex) => (
             <div>
@@ -82,9 +115,9 @@ const KeywordSearchPage = () => {
                 {upperIndex === 2 && "< In Synonyms >"}
               </h3>
               {_.isEmpty(results) && <p>Nothing</p>}
-              <ul>
+              <ul className="sector">
                 {results.map((result, index) => (
-                  <li
+                  <li className="result-list"
                     key={result.filename}
                     onClick={() => handleItemClick(upperIndex, index)}
                   >
