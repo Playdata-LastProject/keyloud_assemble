a72c400cd8e63a5de669c128607b6baad78b6631
RhaHyunAh <rhahyeona@kakao.com>
2024-02-14 03:24:11 +0900

    dealing audio

--- frontend/src/components/ScriptDisplay.js
+++ frontend/src/components/ScriptDisplay.js
@@ -78,29 +78,13 @@ const ScriptDisplay = () => {
     //tmp.srcObject = audioStream;
     //tmp.play(); //simple play of an audio element.
 
-    if (audioData) {
-      const bytesPerSample = 2; // 16비트 = 2바이트
-      const sampleRate = 44100;
-
-      const audioContext = new AudioContext();
-      const audioBuffer = audioContext.createBuffer(
-        1,
-        audioData.length / bytesPerSample,
-        44100
-      );
-      const audioSource = audioContext.createBufferSource();
-      const channelData = audioBuffer.getChannelData(0);
-
-      for (let i = 0; i < audioData.length; i += bytesPerSample) {
-        // 16비트 정수 값을 -1과 1 사이의 부동소수점 값으로 변환
-        const int16Value = (audioData[i + 1] << 8) | (audioData[i] & 0xff);
-        channelData[i / bytesPerSample] = int16Value / 32768.0; // 정규화
-      }
-
-      //audioBuffer.copyToChannel(audioData, 0);
-      audioSource.buffer = audioBuffer;
-      audioSource.connect(audioContext.destination);
-      audioSource.start();
+    if (audioData && Content.mimeType) {
+      const audioBlob = new Blob([audioData], { type: Content.mimeType });
+      const audioUrl = URL.createObjectURL(audioBlob);
+      const audio = new Audio(audioUrl);
+      audio.play();
+    } else {
+      console.error("Audio data not available");
     }
   };
 
