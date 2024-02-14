6a25fb3d358642981c38ececfd0fa3f6f7027d8a
RhaHyunAh <rhahyeona@kakao.com>
2024-02-14 09:29:12 +0900

    dealing audio

--- frontend/src/components/ScriptDisplay.js
+++ frontend/src/components/ScriptDisplay.js
@@ -2,7 +2,6 @@ import { useState, useEffect } from "react";
 import { useLocation } from "react-router-dom";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
-import AudioPlayer from "react-audio-player";
 import "./styles/ScriptDisplay.css";
 import AudioPlayer from "react-audio-player";
 
