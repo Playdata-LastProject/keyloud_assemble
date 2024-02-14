de4b0df02e15ad9f0a612056ebbbd7a0d932411c
RhaHyunAh <rhahyeona@kakao.com>
2024-02-14 09:52:42 +0900

    dealing audio

--- backend/server.js
+++ backend/server.js
@@ -135,7 +135,7 @@ app.post("/upload_files", multer().single("files"), async (req, res) => {
       keywords: keywords_result,
       synonyms: synonyms_result,
       timestamp: timestamp_result,
-      link: copy_path.replace(/\.[^/.]+$/, "_linear16_2.wav"),
+      //link: copy_path.replace(/\.[^/.]+$/, "_linear16_2.wav"),
       // 기타 필요한 파일 정보들 ..추가 -> erd보고 추가
     };
 
@@ -404,7 +404,7 @@ app.get("/contents", async (req, res) => {
       keywords: 0,
       synonyms: 1,
       timestamp: 1,
-      link: 1,
+      //link: 1,
     };
 
     const content = await collection.findOne(
